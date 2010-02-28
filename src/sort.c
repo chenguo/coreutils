aa66d9b3fea44ca71725ea5873f8606a85dc8dff
Joey Degges <jdegges@gmail.com>
2010-02-28 14:42:20 -0800

    Replaced fmin with MIN.

--- src/sort.c
+++ src/sort.c
@@ -3003,7 +3003,7 @@ sort (char * const *files, size_t nfiles, char const *output_file,
           // Cap to 16x nthreads
           // This is just a general heuristic. Once you have more threads
           // running, CPU likely becomes the bottleneck rather than IO
-          unsigned long int num_threads_to_use = (unsigned long int)fmin(num_devices, 16 * nthreads)-1;
+          unsigned long int num_threads_to_use = (unsigned long int) MIN (num_devices, 16 * nthreads)-1;
           unsigned long int num_subthreads_per_thread = floor (nthreads / num_threads_to_use);
           pthread_t *threads = (pthread_t *)malloc (num_threads_to_use * sizeof (pthread_t));
           unsigned long int thread_num = 0;
