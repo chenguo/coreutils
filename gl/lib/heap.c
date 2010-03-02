b4e7fa1412a708e25971283c3ae4a07cb6e6da25
Chen Guo <chenguo4@yahoo.com>
2010-03-02 00:37:28 -0800

    License updates.
    gl/lib/heap.c: update license.
    gl/lib/heap.h: update license.
    gl/lib/memcoll_nul.c: change copyright date.
    gl/lib/xmemcoll_nul.c: change coypright date.
    gl/lib/memcoll_nul: add Chen Guo as maintainer.
    gl/lib/xmemcoll_nul: add Chen Guo as maintainer.

--- gl/lib/heap.c
+++ gl/lib/heap.c
@@ -1,27 +1,22 @@
-/*
- * This file is part of the Generic Data Structures Library (GDSL).
- * Copyright (C) 1998-2006 Nicolas Darnis <ndarnis@free.fr>.
- *
- * The GDSL library is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * The GDSL library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with the GDSL library; see the file COPYING.
- * If not, write to the Free Software Foundation, Inc., 
- * 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
- *
- * $RCSfile: gdsl_heap.c,v $
- * $Revision: 1.13 $
- * $Date: 2006/06/21 14:47:24 $
- */
+/* Barebones heap implementation supporting only insert and pop.
 
+   Copyright (C) 2010 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Full implementation: GDSL (http://gna.org/projects/gdsl/) by Nicolas
+   Darnis <ndarnis@free.fr>. Adapted by Gene Auyeung. */
 
 #include <assert.h>
 #include "stdlib--.h"
